3c76ab6c7fcbdaecd4c0da98de370a69a2d3ac37
xpossed-you <coolgamerindo@gmail.com>
2024-01-25 13:53:29 +0800

    pisah <script> dari index.html ke script.js biar gampang

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.body.addEventListener('click', function(event) {
+        if (event.target && event.target.id === 'toggleButton') {
+            var toggleButton = event.target;
+            var navLinks = document.querySelectorAll('.nav-link-item');
+
+            navLinks.forEach(function(link, index) {
+                if (link.classList.contains('d-none')) {
+                    setTimeout(function() {
+                        link.classList.remove('d-none', 'animated-navbar-out');
+                        link.classList.add('animated-navbar-in');
+                    }, (navLinks.length - index - 1) * 100);
+                } else {
+                    link.classList.add('animated-navbar-out');
+
+                    link.addEventListener('animationend', function() {
+                        link.classList.add('d-none');
+                        link.classList.remove('animated-navbar-in', 'animated-navbar-out');
+                    }, { once: true });
+                }
+            });
+
+            toggleButton.classList.toggle('rotate');
+        }
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('webcomponent/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading navbar:', error);
+        });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('webcomponent/footerbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footerbar-placeholder').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading navbar:', error);
+        });
+});
